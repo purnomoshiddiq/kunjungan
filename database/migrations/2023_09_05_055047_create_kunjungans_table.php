ef6ec06599492f5e93a59bfbf9b9e4088160b643
Purnomo Shiddiq <purnomoshiddiq@gmail.com>
2023-09-05 13:04:59 +0700

    tgl buat frontend

---
+++ database/migrations/2023_09_05_055047_create_kunjungans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kunjungans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('keperluan');
+            $table->date('tgl');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kunjungans');
+    }
+};
